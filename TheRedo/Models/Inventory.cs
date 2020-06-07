6ca1ee204a29fe1cc61a204005542d9a0111e640
Bradley Krishna <21622006@dut4life.ac.za>
2020-06-07 22:10:46 +0200

    test

---
+++ TheRedo/Models/Inventory.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+namespace TheRedo.Models
+{
+    public class Inventory
+    {
+        [Key]
+        public int InventoryID { get; set; }
+        [Required]
+        public string InventoryType { get; set; }
+        [Required]
+        public string InventoryName { get; set; }
+        [Required]
+        public string InventoryDescription { get; set; }
+        [Required]
+        public double InventoryAmount { get; set; }
+        [Required]
+        public bool InventoryStatus { get; set; }
+    }
+}
